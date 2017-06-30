83d4e3f2b2fd7f1b81d4854a16c0252f7a6d504e
李杰 <lj706517903@163.com>
2017-06-30 11:44:06 +0800

    Update ScanQRView.js

--- ScanQRView.js
+++ ScanQRView.js
@@ -1,35 +1,39 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   Text,
-  View , 
-  Dimensions , 
-  Vibration , 
-  Animated , 
-  Easing , 
-  Alert 
+  View ,
+  Dimensions ,
+  Vibration ,
+  Animated ,
+  Easing ,
+  Alert
 } from 'react-native';
 import Camera from 'react-native-camera';
 export default class ScanQRView extends Component {
-  
+    static propTypes = {
+        scanColor: React.PropTypes.string,
+        scanRectWidth: React.PropTypes.number,
+    }
+    static defaultProps = {
+        scanColor: '#1dacf9',
+        scanRectWidth : 200,
+    }
   constructor(props){
       super(props) ;
     this.state={
-      scanTranslte:new Animated.Value(0) 
+      scanTranslte:new Animated.Value(0)
     }
   }
   componentDidMount(){
-    
+
     this.scanLineAnimation();
   }
   scanLineAnimation(){
+      const {
+          scanRectWidth,
+      } = this.props
     Animated.sequence([
-      Animated.timing(this.state.scanTranslte,{toValue:200 , duration:3000 , easing:Easing.linear}),
+      Animated.timing(this.state.scanTranslte,{toValue:scanRectWidth , duration:3000 , easing:Easing.linear}),
       Animated.timing(this.state.scanTranslte,{toValue:0 , duration:3000 , easing:Easing.linear})
                        ]).start(()=>this.scanLineAnimation()) ;
   }
@@ -43,43 +47,43 @@ export default class ScanQRView extends Component {
         this.props.onBarCodeRead(result , ()=>{this.camera.shouldQR();});
     }
     else
-      Alert.alert( "扫描结果" , result.data , [{text:"ok" , onPress:()=>this.camera&&this.camera.shouldQR()}] , {cancelable:false}) ; 
-    
+      Alert.alert( "扫描结果" , result.data , [{text:"ok" , onPress:()=>this.camera&&this.camera.shouldQR()}] , {cancelable:false}) ;
+
   }
   render() {
-    let width = Dimensions.get('window').width ; 
+      const {
+          scanColor,
+          scanRectWidth,
+      } = this.props
+    let width = Dimensions.get('window').width ;
     let height = Dimensions.get('window').height ;
     let scanCoverColor = '#44444488' ;
-    let scanRectWidth = 200 ;
-    console.log("render size = " + width +" height = " + height ) ;
     return (
       <View style={{flex:1}}>
-        <Camera 
+        <Camera
           style={{width:width , height:height}}
           type={Camera.constants.Type.back}
           ref={(cam) => {
             this.camera = cam;
           }}
           onBarCodeRead={this.onBarCodeRead.bind(this)}
-          /> 
+          />
         <View
           style={{position:'absolute' , left:0 , right:0, top:0 , bottom:0}}
           >
-          <View style={{width:width , height:scanRectWidth /2, backgroundColor:scanCoverColor}}/>
-          <View style={{width:width , height:scanRectWidth ,flexDirection:'row'}}>
-            <View style={{flex:1,backgroundColor:scanCoverColor}}/>
-            <View style={{width:scanRectWidth}}>
-              <Animated.View style={{width:scanRectWidth , height:2 , backgroundColor:"#1dacf9" , transform:[
-                                     {translateY:this.state.scanTranslte}
-                                   ] }}/>
-            </View>
-            <View style={{flex:1 , backgroundColor:scanCoverColor}}/>
-            
-          </View>
+          <View style={{flex:1, backgroundColor:scanCoverColor}}/>
+              <View style={{width:width , height:scanRectWidth ,flexDirection:'row'}}>
+                <View style={{flex:1,backgroundColor:scanCoverColor}}/>
+                <View style={{width:scanRectWidth,borderWidth:1,borderColor:'#fff'}}>
+                  <Animated.View style={{width:scanRectWidth , height:2 , backgroundColor:scanColor , transform:[
+                                         {translateY:this.state.scanTranslte}
+                                       ] }}/>
+                </View>
+                <View style={{flex:1 , backgroundColor:scanCoverColor}}/>
+              </View>
           <View style={{flex:1 , width:width , backgroundColor:scanCoverColor}}/>
         </View>
       </View>
     );
   }
 }
-
